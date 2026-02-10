5cc73d31663d74a78271661608fbdb53089e219c
Praveen Kumar V <praveen.kumar2@bounteous.com>
2026-02-10 18:58:57 +0530

    Updated vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// Replace REPO_NAME with your GitHub repo name
+const REPO_NAME = "praveen-kumar-bounteous";
+
 export default defineConfig({
+  base: `/${REPO_NAME}/`, 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
